3c129b53a6e6664c2ef916d7d9da49333d925bf0
Neil <ns532@cam.ac.uk>
2013-02-14 03:53:35 +0000

    Make 'next' optional
    The auth module takes 'next' as an optional parameter. If this is not
    passed, it checks if the settings specify a 'LASTFM_AUTH_REDIRECT' to
    redirect to. If not, it redirects to '/admin/'

--- lastfmauth/views.py
+++ lastfmauth/views.py
@@ -11,10 +11,13 @@ API_KEY = settings.LASTFM_API_KEY
 
 def do_login(request):
     login_complete_url = request.build_absolute_uri(reverse('lastfm_login_complete'))
-    next = request.GET["next"]
-    redirecturl = "%s?next=%s" % (login_complete_url , next)
-    parameters = {'api_key': API_KEY, 'cb': redirecturl}
+    parameters = {'api_key': API_KEY}
 
+    if ('next' in request.GET):
+	    next = request.GET["next"]
+	    redirecturl = "%s?next=%s" % (login_complete_url , next)
+	    parameters['cb'] = redirecturl
+		
     signin_url = "http://www.last.fm/api/auth/?%s" % urlencode(parameters)
     return HttpResponseRedirect(signin_url)
 
@@ -25,6 +28,10 @@ def login_complete(request):
     if user:
         login(request, user)
 
-    next = request.GET["next"]
-
-    return HttpResponseRedirect(next)
+    if ('next' in request.GET):
+    	next = request.GET["next"]
+    	return HttpResponseRedirect(next)
+    elif hasattr(settings, 'LASTFM_AUTH_REDIRECT'):
+    	return HttpResponseRedirect(settings.LASTFM_AUTH_REDIRECT)
+    else:
+    	return HttpResponseRedirect('/admin/')
