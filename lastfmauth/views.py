103d5d2bd8d99c9927f883732c30857672ba2d8b
Neil <ns532@cam.ac.uk>
2013-02-14 03:39:38 +0000

    Add support for post-login redirect
    Sending next parameter to login ensures you end up at the same page
    after logging in.

--- lastfmauth/views.py
+++ lastfmauth/views.py
@@ -2,13 +2,20 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
+from urllib import urlencode
+from django.core.urlresolvers import reverse
 
 from lastfmauth.models import LastfmProfile
 
 API_KEY = settings.LASTFM_API_KEY
 
 def do_login(request):
-    signin_url = "http://www.last.fm/api/auth/?api_key=%s" % API_KEY
+    login_complete_url = request.build_absolute_uri(reverse('lastfm_login_complete'))
+    next = request.GET["next"]
+    redirecturl = "%s?next=%s" % (login_complete_url , next)
+    parameters = {'api_key': API_KEY, 'cb': redirecturl}
+
+    signin_url = "http://www.last.fm/api/auth/?%s" % urlencode(parameters)
     return HttpResponseRedirect(signin_url)
 
 def login_complete(request):
@@ -18,4 +25,6 @@ def login_complete(request):
     if user:
         login(request, user)
 
-    return HttpResponseRedirect("/")
+    next = request.GET["next"]
+
+    return HttpResponseRedirect(next)
