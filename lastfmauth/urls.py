334a8189b99c0917615797deb28eb7d3a8eb2112
Neil <ns532@cam.ac.uk>
2013-03-03 01:29:38 +0000

    Fix url name for logout to be consistent with login.

--- lastfmauth/urls.py
+++ lastfmauth/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns("lastfmauth.views",
     url('^login/$', "do_login", name='lastfmauth_login'),
     url('^login_complete/$', "login_complete", name='lastfm_login_complete'),
-    url('^logout/$', "do_logout", name="lastfm_logout")
+    url('^logout/$', "do_logout", name="lastfmauth_logout")
 )
