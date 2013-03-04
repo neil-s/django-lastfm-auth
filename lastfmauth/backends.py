2a7088aef111169198de4cba51d18a591d7a7fb5
Neil <ns532@cam.ac.uk>
2013-03-04 05:07:38 +0000

    Redirect new users to artists page if they logged in/signed up from the home page.

--- lastfmauth/backends.py
+++ lastfmauth/backends.py
@@ -1,12 +1,8 @@
 # -*- coding: UTF-8 -*-
-from hashlib import md5
-from urllib2 import urlopen
 
 from django.conf import settings
 from django.contrib.auth.models import User
 
-# from lastfmauth.models import LastfmProfile
-from se_api import ScrobbleExchange
 
 API_KEY = settings.LASTFM_API_KEY
 WS_URL = settings.LASTFM_WS_BASE_URL
@@ -20,15 +16,16 @@ class LastfmAuthBackend:
         authuser = client.login(token=token)
 
         # try to find a user instance with a matching Authuser
-        user, user_created = User.objects.get_or_create(username = authuser.name.name,
-                                                         first_name=authuser.session_key
-                                                         )
-        #^ Hack using superfluous first_name field to store session key to avoid extra profile class
-           
+        user, user_created = User.objects.get_or_create(    username=authuser.name.name,
+                                                            first_name=authuser.session_key
+                                                        )
+        #^ Hack using superfluous first_name field to store session key to avoid extra profile class.
+
         if user_created:
             user.set_password(User.objects.make_random_password())
             user.save()
- 
+        user.authuser = authuser
+
         return user
 
     def get_user(self, user_id):
--- lastfmauth/views.py
+++ lastfmauth/views.py
@@ -1,39 +1,48 @@
 # -*- coding: UTF-8 -*-
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from urllib import urlencode
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 
 API_KEY = settings.LASTFM_API_KEY
 
+
 def do_login(request):
     login_complete_url = request.build_absolute_uri(reverse('lastfm_login_complete'))
     parameters = {'api_key': API_KEY}
 
     if ('next' in request.GET):
-	    next = request.GET["next"]
-	    redirecturl = "%s?next=%s" % (login_complete_url , next)
-	    parameters['cb'] = redirecturl
-		
+        next = request.GET["next"]
+        redirecturl = "%s?next=%s" % (login_complete_url, next)
+        parameters['cb'] = redirecturl
+
     signin_url = "http://www.last.fm/api/auth/?%s" % urlencode(parameters)
     return HttpResponseRedirect(signin_url)
+
 
 def login_complete(request):
     token = request.GET["token"]
 
+    # user, new_user = authenticate(token=token)
     user = authenticate(token=token)
+    new_user = user.authuser.newuser
     if user:
         login(request, user)
 
+    #Redirect to next, unless new user and next is home page.
+    #If next is not set, redirect based on setting or to home page.
     if ('next' in request.GET):
-    	next = request.GET["next"]
-    	return HttpResponseRedirect(next)
+        next = request.GET["next"]
+        if new_user and next == "/":
+            redirect('artists')
+        return HttpResponseRedirect(next)
     elif hasattr(settings, 'LASTFM_AUTH_REDIRECT'):
-    	return HttpResponseRedirect(settings.LASTFM_AUTH_REDIRECT)
+        return HttpResponseRedirect(settings.LASTFM_AUTH_REDIRECT)
     else:
-    	return HttpResponseRedirect('/admin/')
+        return HttpResponseRedirect('/')
+
 
 def do_logout(request):
     logout(request)
